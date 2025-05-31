c7df3d9370404041131eb33a9b6f046267004d9b
s890126 <ss26214555@gmail.com>
2025-05-31 22:43:01 +0800

    Initial commit

---
+++ src/main/java/com/tzuwang/spotify_llm_bot/SpotifyLlmBotApplication.java
@@ -0,0 +1,13 @@
+package com.tzuwang.spotify_llm_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyLlmBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyLlmBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tzuwang/spotify_llm_bot/SpotifyLlmBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzuwang.spotify_llm_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyLlmBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
